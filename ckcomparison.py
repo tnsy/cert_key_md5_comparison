3335bc479097e42fbcf8c63fee306c55d34e8812
tnsy <tokacprzak@gmail.com>
2016-02-19 09:25:44 +0100

    shortened + unused certs
    Shortened the code and added Certs without pair

--- ckcomparison.py
+++ ckcomparison.py
@@ -6,7 +6,7 @@ list_of_keys = glob.glob("/config/filestore/files_d/*/certificate_key_d/*")
 
 cert_dict = {}
 key_dict = {}
-intersection = []
+failed_certs = []
 
 print '\n', ' CERTIFICATES FOUND '.center(100, '='), '\n\n'
 for c in list_of_certificates:
@@ -25,12 +25,15 @@ for key in list_of_keys:
     key_hash = b.read()
     key_dict[key_hash] = key
 
+print '\n', ' CERTS - KEYS MATCH '.center(100, '=')
+
 for k in cert_dict.keys():
     if key_dict.has_key(k):
-        intersection.append(k)
-
-print '\n', ' CERTS - KEYS MATCH '.center(100, '=')
-for i in intersection:
-    if i in cert_dict:
-        print '\n', 'Cer: ', cert_dict[i].rjust(94), '\nKey: ', key_dict[i].rjust(94), '\n'
+        print '\n', 'Cer: ', cert_dict[k].rjust(94), '\nKey: ', key_dict[k].rjust(94), '\n'
         print '-' * 100
+    else:
+        failed_certs.append(cert_dict[k])
+
+print '\n', ' CERTS WITH NO MATCH '.center(100, '='), '\n\n'
+for cert in failed_certs:
+    print 'Cer: ', cert.rjust(94)
